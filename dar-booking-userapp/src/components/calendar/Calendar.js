b0cb719e79b1d911c03e42dc7070bd09d60ddad7
daryn2 <amantaevdaryn@gmail.com>
2020-04-04 00:59:44 +0600

    calendar added

--- dar-booking-userapp/src/components/calendar/Calendar.js
+++ dar-booking-userapp/src/components/calendar/Calendar.js
@@ -1,34 +1,57 @@
 import React, { Component } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
-
 import "react-big-calendar/lib/css/react-big-calendar.css";
-
+import Meeting from '../meeting/Meeting';
+import RoomsModal from "../modal/modal";
 const localizer = momentLocalizer(moment);
 
 class RoomsCalendar extends Component {
+
   state = {
-    events: [
-      {
-        start: moment().toDate(),
-        end: moment()
-          .add(1, "days")
-          .toDate(),
-        title: "Some title"
-      }
-    ]
-  };
+    "id": 1
+  }
+
+  constructor() {
+    super();
+    this.child = React.createRef();
+  }
+  eventHandler = (e) => {
+    console.log("there");
+    console.log(e);
+    console.log(e.id)
+    this.setState({
+      id: e.id
+    });
+    this.child.current.showModal();
+  }
 
   render() {
+    let events = []
+    for (let i = 0; i < this.props.meetings.length; i++) {
+      if (this.props.meetings[i] !== undefined) {
+        let meetings_time = {
+          id: this.props.meetings[i].id,
+          start: new Date(this.props.meetings[i].startTime),
+          end: new Date(this.props.meetings[i].endTime),
+          title: this.props.meetings[i].title
+        }
+        events.push(meetings_time)
+      }
+    }
     return (
       <div className="RoomsCalendar">
         <Calendar
           localizer={localizer}
           defaultDate={new Date()}
-          defaultView="month"
-          events={this.state.events}
-          style={{ height: "100vh" }}
+          defaultView="day"
+          events={events}
+          style={{ height: "600px" }}
+          onSelectEvent={this.eventHandler}
         />
+        <RoomsModal ref={this.child}>
+          <Meeting id={this.state.id} />
+        </RoomsModal>
       </div>
     );
   }
