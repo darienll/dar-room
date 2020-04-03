b5b86d25d870b79bda6c1170cd826222d76876bb
daryn2 <amantaevdaryn@gmail.com>
2020-04-04 01:00:10 +0600

    room added

--- dar-booking-userapp/src/components/room/Room.js
+++ dar-booking-userapp/src/components/room/Room.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import './Room.css';
-import room_img from '../../assets/room1.jpg';
-import Modal from 'react-modal';
-import RoomsModal from '../modal/modal';
+import { Button } from 'antd';
+import { Link } from 'react-router-dom';
 
 function Room({ item }) {
     return (
         <div className="room">
-            <div className="room__image">   
-            <figure>
-                <img src={ room_img } width="100%"/>
-            </figure>
+            <div className="room__image">
+                <figure>
+                    <img src={process.env.REACT_APP_HOST + item.imageUrl} alt="room" width="100%" />
+                </figure>
             </div>
             <div className="room__text">
-                <h4>{ item.name } </h4>
+                <h3>{item.name} </h3>
+                <Button>
+                    <Link to={'/book/' + item.id}>Забронировать</Link>
+                </Button>
             </div>
-            <RoomsModal room = { item }/>
         </div>
     )
 }
