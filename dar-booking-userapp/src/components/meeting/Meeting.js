ffff95644910f705b2e0c533c3cdd22eb9541002
daryn2 <amantaevdaryn@gmail.com>
2020-04-04 00:59:53 +0600

    meeting added

---
+++ dar-booking-userapp/src/components/meeting/Meeting.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import Moment from 'moment';
+
+function Meeting({ id }) {
+    useEffect(() => {
+        const getUsers = () => {
+            const host = process.env.REACT_APP_HOST + 'users/meetings/' + id;
+            console.log(host);
+            fetch(host)
+                .then((res) => {
+                    return res.json()
+                })
+                .then((res) => {
+                    console.log(res);
+                    setMeeting(res);
+                    setParticipants(res.participants)
+                })
+        }
+        getUsers();
+    }, [id])
+    const [meetings, setMeeting] = useState([]);
+    const [participants, setParticipants] = useState([])
+    return (
+        <div className="meeting">
+            <div className="heading">
+                <h2>
+                    {meetings.title}
+                </h2>
+            </div>
+            <div className="content">
+                <p> Start:  {Moment(meetings.startTime).format('YYYY-MM-DD hh:mm a')} </p>
+                <p> End: {Moment(meetings.endTime).format('YYYY-MM-DD hh:mm a')}</p>
+
+                {(participants.length > 0) ? (
+                    <div>
+                        <p>Participants:</p>
+                        <ul>
+                            {participants.map(participant => (
+                                <li key={participant.id}> {participant.firstName}</li>
+                            ))
+                            }
+                        </ul>
+                    </div>
+                ) : (<div></div>)
+
+                }
+            </div>
+        </div>
+    )
+}
+export default Meeting;
